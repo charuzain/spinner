6ee2fc251513ca4864e85bacb3831f28d163caa2
charuzain <charuabhizain@gmail.com>
2021-09-23 18:52:50 -0400

    Loading spinner using setTimeOut

---
+++ spinner1.js
@@ -0,0 +1,39 @@
+//process.stdout.write('hello from spinner1.js...\rheyyy\n');
+setTimeout(() => {
+  process.stdout.write('\r|    ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/    ');
+}, 400);
+
+setTimeout(() => {
+  process.stdout.write('\r-    ');
+}, 700);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\    ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r|    ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r/    ');
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r-    ');
+}, 2100);
+
+setTimeout(() => {
+  process.stdout.write('\r\\    ');
+}, 2500);
+
+setTimeout(() => {
+  process.stdout.write('\r|    \n');
+}, 2900);
+
+
